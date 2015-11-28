1ae462b9c25afa5e5b28fc627121b510d780bd9e
jorgesaenz <jorge.asr@outlook.com>
2015-11-27 22:19:04 -0600

    Revert "libs y login"
    This reverts commit 65419f1faec165ea88f8ebbac05227d3dea63573.

--- ShoutOut/library/setting.java
+++
@@ -1,6 +0,0 @@
-package test.Droidlogin.library;
-
-public class setting{
-	final public static String IP_Server = "192.168.0.5";
-    final public static String URL_connect="http://"+IP_Server+"/droidlogin";
-}
